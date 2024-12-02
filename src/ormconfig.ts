cb6f9f9e8bebade24b91bdde849823c2e587a4d3
YarikMichkivskyi <yaroslav.michkivskyi@gmail.com>
2024-12-02 22:25:43 +0200

    + ormconfig mini-fix

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -2,15 +2,19 @@ import {DataSource} from 'typeorm';
 import {User} from "./users/users.entity";
 import {Exhibit} from "./exhibits/exhibits.entity";
 import {Comment} from "./comments/comments.entity";
+import {config} from 'dotenv'
+
+config();
 
 export default new DataSource({
     type: 'postgres',
-    host: 'localhost',
+    host: process.env.DB_HOST,
     port: 5432,
-    username: 'exhibits_admin',
-    password: 'password',
-    database: 'exhibits_db',
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     migrations: ['./src/migrations/*.ts'],
     synchronize: false,
     entities: [User, Comment, Exhibit]
 });
+
